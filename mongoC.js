6f094d4f531b150d88e7440240333699cfed9da3
muruganantham <muruganantham2399@gmail.com>
2025-05-22 21:38:51 +0530

    initial-commit

--- mongoC.js
+++ mongoC.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.hu5pjmi.mongodb.net/?retryWrites=true&w=majority`; // cluster url
+const connectionString = `mongodb+srv://muruganantham2399:${password}@devcluster.nt08unt.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster`; 
 const client = new MongoClient(connectionString);
 let conn;
 try {
